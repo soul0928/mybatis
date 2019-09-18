afc7b0124a87cdaebaf0513a8b5a7eda10d66d5a
wangdong <wangdong@fescotech.com>
2019-09-18 16:22:35 +0800

    修改 Mysql 为5.7版本， 简单添加JDBC连接Mysql

---
+++ src/test/java/com/liuyun/mapper/UserrMapper.java
@@ -0,0 +1,18 @@
+package com.liuyun.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ProjectName mybatis
+ * @ClassName UserrMapper
+ * @Description
+ * @Author WangDong
+ * @Date 2019/9/18 15:55
+ * @Version 2.1.3
+ **/
+public interface UserrMapper {
+
+  List<Map> queryAll();
+
+}
